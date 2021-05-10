962c1b4f89eb5d96268ca09e91862fdfe47885c5
flu5hguy <parano1800@gmail.com>
2021-05-10 19:45:40 +0430

    Added configs

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,17 @@ const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
   // Customize the config before returning it.
-  const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-  //const  HtmlWebpackPlugin  = require('html-webpack-plugin') ; 
+  // const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+  // //const  HtmlWebpackPlugin  = require('html-webpack-plugin') ; 
 
 
-  if (env.mode === 'production') {
-    config.plugins.push(
-      new BundleAnalyzerPlugin({
-        path: 'web-report',
-      })
-    );
-  }
+  // if (env.mode === 'production') {
+  //   config.plugins.push(
+  //     new BundleAnalyzerPlugin({
+  //       path: 'web-report',
+  //     })
+  //   );
+  // }
 
   // config.plugins.push(
   //   new HtmlWebpackPlugin({
